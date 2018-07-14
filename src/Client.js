e6bac45eb5b39f1158033c6e2176858abb26277b
Desvelao <iamdesvelao@gmail.com>
2018-07-14 11:29:44 +0200

    update 0.8.6 eris + msg.replyDM

--- src/Client.js
+++ src/Client.js
@@ -5,9 +5,17 @@ const Watcher = require('./Watcher');
 const reload = require('require-reload')(require);
 const extensions = require('./extensions')
 
-Eris.Message.prototype.reply = function(message,file){ const self = this; return new Promise(function(resolve,reject){
-	self.channel.createMessage(message,file).then(m => resolve(m))
-})}
+Eris.Message.prototype.reply = function(message,file){
+	return new Promise((resolve,reject) => {
+		this.channel.createMessage(message,file).then(m => resolve(m)).catch(err => reject(err))
+	})
+}
+
+Eris.Message.prototype.replyDM = function(content,file){
+  return new Promise((resolve,reject) => {
+    this.author.getDMChannel().then(channel => channel.createMessage(content,file)).then(m => resolve(m)).catch(err => reject(err))
+  })
+}
 // const LoggerThing = require('./util.js');
 let u;
 
