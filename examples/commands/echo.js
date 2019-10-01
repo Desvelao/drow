bddb102239d2988b1d94fac18baa85ccd026395b
Desvelao <iamdesvelao@gmail.com>
2019-10-01 20:01:08 +0200

    update command examples

--- examples/commands/echo.js
+++ examples/commands/echo.js
@@ -3,7 +3,6 @@ const { Command } = require('aghanim')
 module.exports = new Command('echo',{
   category : 'Owner', help : 'Echo a message', args : '',
   ownerOnly : true, hide : true},
-  function(msg, args, command){
-    // let self = this //this representing Aghanim.Client
+  function(msg, args, client, command){
     return msg.channel.createMessage(args.from(1))
   })
