980e0bc6969ab74613757c1d9a1b2c60053e8997
Desvelao <iamdesvelao@gmail.com>
2019-10-16 02:18:26 +0200

    fix member has role

--- src/requirements/builtin/member.has.role.js
+++ src/requirements/builtin/member.has.role.js
@@ -1,14 +1,21 @@
-module.exports = ({role, response, responseDM, run}) => ({
+module.exports = ({role, response, responseDM, run, incaseSensitive}) => ({
     condition: (msg, args, client, command, req) => {
         if(!msg.channel.guild){return false}
         const member = msg.channel.guild.members.get(msg.author.id)
         if(!member){return false}
         let { role } = req
-        role = role.map(r => r.toLowerCase())
-		return member.roles.some( r => roles.includes(msg.channel.guild.roles.get(r).name.toLowerCase()) )
+        role = role.map(r => toInCaseSensitive(r, req.incaseSensitive))
+        return member.roles.some( r => 
+            role.includes( 
+                toInCaseSensitive(msg.channel.guild.roles.get(r).name, req.incaseSensitive)
+            ) 
+        )
     },
     role: Array.isArray(role) ? role : [role],
     response,
     responseDM,
-    run
+    run,
+    incaseSensitive
 })
+
+const toInCaseSensitive = (text, incase) => incase ? text.toLowerCase() : text
